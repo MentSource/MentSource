f550857b3152a20e84ded50d77444d10de4a50fe
Peculiar C. Umeh <35475543+peculiaruc@users.noreply.github.com>
2026-04-04 09:26:34 +0100

    Update contributors field in ProgramsPreview.tsx

---
+++ src/components/home/ProgramsPreview.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ProgramsPreview = () => {
+    return (
+        <div>
+            {/* Other components or HTML */}
+            <div>
+                <h2>Contributors: 5</h2> {/* Updated contributors field */}
+            </div>
+        </div>
+    );
+};
+
+export default ProgramsPreview;
